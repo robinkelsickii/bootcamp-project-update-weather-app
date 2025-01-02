5f10c8bd0c861672f03319a43391dae3c879ebb5
Robin Kelsick II <robinkelsickii@gmail.com>
2025-01-02 17:47:14 +0000

    added card that is transparent and displays imperial units

--- weather-app/src/components/WeatherDisplay.jsx
+++ weather-app/src/components/WeatherDisplay.jsx
@@ -1,29 +1,45 @@
 function WeatherDisplay({ weatherData, location }) {
-  const { sys, main, weather, wind } = weatherData;
-  const temperature = main.temp;
-  const humidity = main.humidity;
-  const windSpeed = wind.speed;
-  const weatherDescription = weather[0].description;
+  const { sys, main, weather = [{}], wind } = weatherData;
+  const temperature = main?.temp || "N/A";
+  const humidity = main?.humidity || "N/A";
+  const windSpeed = wind?.speed || "N/A";
+  const weatherDescription = weather[0]?.description || "Unknown weather";
+  const icon = weather[0]?.icon || "01d";
+  const farenheit = Math.round(temperature * (9 / 5) + 32);
 
-  const cityStateCountry = location.split(',');
 
-  let locationText = location;
-  if (cityStateCountry.length === 3) {
-    const [city, state, country] = cityStateCountry;
-    locationText = `${city}, ${state}, ${country}`;
-  } else {
-    locationText = `${location}, ${sys.country}`;
-  }
+  const cityStateCountry = location?.split(',') || [];
+  const locationText = cityStateCountry.length === 3
+    ? `${cityStateCountry[0]}, ${cityStateCountry[1]}, ${cityStateCountry[2]}`
+    : `${location || "Unknown Location"}, ${sys?.country || ""}`;
+
 
   return (
-    <div className="w-80 p-4 bg-white rounded-lg shadow-md mt-6">
-      <h4 className="text-xl font-semibold">{locationText}</h4>
-      <p className="text-lg">{weatherDescription}</p>
-      <div className="mt-4">
-        <p>Temperature: {temperature}°C</p>
-        <p>Humidity: {humidity}%</p>
-        <p>Wind Speed: {windSpeed} m/s</p>
-      </div>
+    <div className="mt-16">
+      <a
+        href="#"
+        className="relative block overflow-hidden rounded-lg border border-gray-200 p-4 sm:p-6 lg:p-8"
+      >
+        <div className="sm:flex sm:justify-between sm:gap-4">
+          {/* Location Text */}
+          <div className="text-lg font-bold text-gray-900 sm:text-xl">
+            {locationText}
+          </div>
+
+          {/* Weather Icon */}
+          <div className="hidden sm:block sm:shrink-0">
+            <img
+              src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+              alt={`Weather icon showing ${weatherDescription}`}
+            />
+          </div>
+        </div>
+        <div className="mt-4 flex justify-between">
+          <p>{farenheit}°F</p>
+          <p>{temperature}°C</p>
+        </div>
+
+      </a>
     </div>
   );
 }
